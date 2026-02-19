e8ef1e02322164e72107830c6ffd69cfc5012501
qian-o <1324771795@qq.com>
2026-02-19 10:58:09 +0800

    Add property helpers to NativeObject for Obj-C interop
    Added protected GetProperty and SetProperty methods to NativeObject to simplify and type-safely manage Objective-C property access, caching, and assignment using native pointers.

--- Metal.NET/Common/NativeObject.cs
+++ Metal.NET/Common/NativeObject.cs
@@ -10,4 +10,28 @@ public abstract class NativeObject(nint nativePtr)
     /// The underlying Objective-C pointer.
     /// </summary>
     public nint NativePtr { get; } = nativePtr;
+
+    protected T? GetProperty<T>(ref T? field, Selector selector) where T : NativeObject
+    {
+        nint nativePtr = ObjectiveCRuntime.MsgSendPtr(NativePtr, selector);
+
+        if (nativePtr is 0)
+        {
+            return field = null;
+        }
+
+        if (field is null || field.NativePtr != nativePtr)
+        {
+            field = (T)Activator.CreateInstance(typeof(T), nativePtr)!;
+        }
+
+        return field;
+    }
+
+    protected void SetProperty<T>(ref T? field, Selector selector, T? value) where T : NativeObject
+    {
+        ObjectiveCRuntime.MsgSend(NativePtr, selector, value?.NativePtr ?? 0);
+
+        field = value;
+    }
 }
