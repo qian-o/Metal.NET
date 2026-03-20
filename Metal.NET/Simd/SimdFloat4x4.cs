cd966800aa9e6ddf73bea4e99c56369f6c59ce0e
qian-o <1324771795@qq.com>
2026-03-20 15:11:02 +0800

    Refactor Simd structures: move SimdFloat4 and SimdFloat4x4 to separate files for better organization

--- Metal.NET/Simd/SimdFloat4x4.cs
+++ Metal.NET/Simd/SimdFloat4x4.cs
@@ -1,18 +1,6 @@
-﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices;
 
 namespace Metal.NET;
-
-[StructLayout(LayoutKind.Sequential)]
-public struct SimdFloat4(float x, float y, float z, float w)
-{
-    public float X = x;
-
-    public float Y = y;
-
-    public float Z = z;
-
-    public float W = w;
-}
 
 [StructLayout(LayoutKind.Sequential)]
 public struct SimdFloat4x4(SimdFloat4 column0, SimdFloat4 column1, SimdFloat4 column2, SimdFloat4 column3)
